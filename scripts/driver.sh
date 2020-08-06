84efd1d4ff050bb522b2f135bfe852fc20dea0ea
vinay kumar <vinaysharma031@gmail.com>
2020-08-06 14:52:46 +0530

    script update

--- scripts/driver.sh
+++ scripts/driver.sh
@@ -1,18 +1,48 @@
 #!/bin/bash
 
-echo "installing chromedriver and dependency"
-apt-get install -y unzip wget                           
-apt-get install google-chrome-stable
-cd /opt
+#Installing chromedriver for currently installed chrome version
+
+
+#check the install version of chrome and put first three values in a file ver.txt 
+#using this first three value we will get to know to suitable version of chromedriver for the existing chrome version
+
 google-chrome --version | awk {'print $3'} | cut -d "." -f 1-3 > /opt/ver.txt
+
+#move to the file location that contain chrome version first three value 
+cd /opt 
+
+#Assign a veriable to that file ver.txt
 VERSION=$(cat /opt/ver.txt)
+
+#show the output of file using variable 
 echo $VERSION
+
+#Remove the file name start with LATEST_RELEASE because we need to download new file
 rm -rf /opt/LATEST_RELEASE_*
+
+#Download a file using this link which contain the version no. of chromedrive which is compatable with the installed chrome version 
 wget https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$VERSION
+
+#Assign a variable to this file 
 DRIVER_VERSION=$(cat /opt/LATEST_RELEASE_*)
+
 echo $DRIVER_VERSION                                              
+
+#Now download the exact chromedriver using the chromedriver version
 wget https://chromedriver.storage.googleapis.com/${DRIVER_VERSION}/chromedriver_linux64.zip 
+
+#Extract the downloaded zip file
 unzip chromedriver_linux64.zip                 
+
+#Move this file to already added executable path
 mv chromedriver /usr/bin/chromedriver     
+
+#Make the root ownership of the chromedriver
 chown root:root /usr/bin/chromedriver     
-chmod +x /usr/bin/chromedriver   
+
+#Make the chromedriver executable
+chmod +x /usr/bin/chromedriver  
+
+#Path for chrome drive
+LINUX_CHROME="/usr/bin/chromedriver"
+echo $LINUX_CHROME
