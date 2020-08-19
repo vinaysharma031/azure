25bc55e25df2c9a4f01c424c65589e5287be50c4
vinay kumar <vinaysharma031@gmail.com>
2020-08-19 14:33:47 +0530

    script added 1

--- mail.sh
+++ mail.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "Please visit ${env.BUILD_URL} for further information." | mail -s "Job '${env.JOB_NAME}' (${env.BUILD_NUMBER}) success." vinkumar@qasource.com -A hello.txt
+echo "Please visit for further information." | mail -s "Job success." vinkumar@qasource.com -A hello.txt
