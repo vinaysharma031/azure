f7d22de9d21a29ae9949d9abebf22cee290b4f74
vinay kumar <vinaysharma031@gmail.com>
2020-08-20 11:19:05 +0530

    commit 3

---
+++ mail.groovy
@@ -0,0 +1,11 @@
+#!/usr/bin/env groovy 
+
+def exampleMethod() {
+
+mail body: "Please visit ${env.BUILD_URL} for further information.",
+subject: "Job '${env.JOB_NAME}' (${env.BUILD_NUMBER}) success.",
+to: 'vinkumar@qasource.com'
+
+}
+
+return this
--- mail.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo "Please visit for further information." | mail -s "Job success." vinkumar@qasource.com -A hello.txt
