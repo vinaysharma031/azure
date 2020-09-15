e4c8cc3fc7c78926d7db8cc5353a7cf79da4e774
root <root@ip-172-31-45-240.ap-south-1.compute.internal>
2020-09-15 05:15:25 +0000

    mail file updated

--- mail.groovy
+++ mail.groovy
@@ -3,7 +3,7 @@ def email() {
 
 mail body: "Please visit ${env.BUILD_URL} for further information.",
 subject: "Job '${env.JOB_NAME}' (${env.BUILD_NUMBER}) success.",
-to: '$Emails'
+to: 'vinkumar@qasource.com'
 
 }
 
